415c203a9c5835f2e5ea1b1e1926b75b6831f513
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2023-11-02 21:43:20 +0100

    simple prover with separate notary process

--- simple/src/bin/simple_prover.rs
+++ simple/src/bin/simple_prover.rs
@@ -19,10 +19,19 @@ const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 const NOTARY_HOST: &str = "127.0.0.1";
 const NOTARY_PORT: u16 = 8080;
 
+use p256::pkcs8::DecodePrivateKey;
+use std::{env, str};
+
+use tokio::net::TcpListener;
+
+use tlsn_verifier::tls::{Verifier, VerifierConfig};
+
 #[tokio::main]
 async fn main() {
-    // Initialize logging
     tracing_subscriber::fmt::init();
+
+    // Start a local simple notary service
+    start_notary_thread().await;
 
     // A Prover configuration
     let config = ProverConfig::builder()
@@ -224,4 +233,50 @@ async fn build_proof_with_redactions(mut prover: Prover<Notarize>) -> TlsProof {
         session: notarized_session.session_proof(),
         substrings: substrings_proof,
     }
+}
+
+async fn start_notary_thread() {
+    tokio::spawn(async {
+        // Allow passing an address to listen on as the first argument of this
+        // program, but otherwise we'll just set up our TCP listener on
+        // 127.0.0.1:8080 for connections.
+        let addr = env::args()
+            .nth(1)
+            .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+        // Next up we create a TCP listener which will listen for incoming
+        // connections. This TCP listener is bound to the address we determined
+        // above and must be associated with an event loop.
+        let listener = TcpListener::bind(&addr).await.unwrap();
+
+        println!("Listening on: {}", addr);
+
+        // Load the notary signing key
+        let signing_key_str =
+            str::from_utf8(include_bytes!("../../../notary/fixture/notary/notary.key")).unwrap();
+        let signing_key = p256::ecdsa::SigningKey::from_pkcs8_pem(signing_key_str).unwrap();
+
+        // loop {
+        // Asynchronously wait for an inbound socket.
+        let (socket, socket_addr) = listener.accept().await.unwrap();
+
+        println!("Accepted connection from: {}", socket_addr);
+
+        {
+            let signing_key = signing_key.clone();
+
+            // Spawn notarization task to be run concurrently
+            tokio::spawn(async move {
+                // Setup default config. Normally a different ID would be generated
+                // for each notarization.
+                let config = VerifierConfig::builder().id("example").build().unwrap();
+
+                Verifier::new(config)
+                    .notarize::<_, p256::ecdsa::Signature>(socket.compat(), &signing_key)
+                    .await
+                    .unwrap();
+            });
+        }
+        // }
+    });
 }
